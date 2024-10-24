db028ce61cb0dd45d7f0d2b0c3fa19a633df7864
skamshort <andreev16012004@gmail.com>
2024-10-24 13:33:03 +0700

    Add files via upload

---
+++ module 2.3.py
@@ -0,0 +1,15 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+numbers = 0
+while numbers < len(my_list):
+      if my_list[numbers] >= 0:
+        if my_list[numbers] != 0:
+            print(my_list[numbers])
+            numbers = numbers + 1
+        else:
+            break
+
+
+
+
+
+
